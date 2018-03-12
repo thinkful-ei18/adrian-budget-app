e6c134ac7f0a2ea7782c82e25fc34764638bd019
Adrian <adriantoddross@gmail.com>
2018-03-12 17:15:30 -0400

    render list items based on array

--- src/components/bills-list.js
+++ src/components/bills-list.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
-export default function billsList (props) {
-  return (
-    <div className="budget-bills-list">
-      <h2>Bills List</h2>
-      <p>Here are all of your bills</p>
-      <ul>
-        <li>Internet $65</li>
-        <li>Utilities $250</li>
-        <li>Groceries $150</li>
-      </ul>
-      <p>Total $465</p>
-      <p>After Bills: $1,535 left</p>
-    </div>
-  );
+export default class billsList extends React.Component {
+
+
+  render () {
+
+    const staticBills = ['Internet $65', 'Groceries $250'];
+
+    const bills = staticBills.map((bill, index) =>
+  <li key={index} className="cheese-list-cheese">
+    {bill}
+  </li>
+);
+
+    return (
+      <div className="budget-bills-list">
+        <h2>Bills List</h2>
+        <p>Here are all of your bills</p>
+        <ul>
+          {bills}
+        </ul>
+        <p>Total $465</p>
+        <p>After Bills: $1,535 left</p>
+      </div>
+    );
+  };
 }
