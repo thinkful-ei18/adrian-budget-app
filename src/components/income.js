789799145d41ccb236e2beabd382082710072581
Adrian <adriantoddross@gmail.com>
2018-03-12 10:46:29 -0400

    add static HTML

--- src/components/income.js
+++ src/components/income.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export default function Income (props) {
-
+return (
+ <div>
+  <h2>Add your monthly income!</h2>
+  <p>Add your expected monthly income.</p>
+    <form onClick={() => console.log('Form clicked!')}>
+      <label htmlFor="Monthly Income">Monthly Income</label>
+      <input type="number" name="Monthly Income"  id=""/>
+      <input type="submit" value=""/>
+    </form>
+ </div>
+);
 }
