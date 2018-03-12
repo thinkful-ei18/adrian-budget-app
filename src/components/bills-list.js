7e3f79a8c21877bd868d52cb34729e7aa48612fc
Adrian <adriantoddross@gmail.com>
2018-03-12 11:44:20 -0400

    add input for bill name

--- src/components/bills-list.js
+++ src/components/bills-list.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 export default function billsList (props) {
   return (
-    <div className="budget-bills">
+    <div className="budget-add-bill">
       <h2>Add A Bill</h2>
       <p>Add a name and dollar amount for your monthly bills</p>
       <form className="add-bill">
-        <label htmlFor="Bill Amont">Bill Amount</label>
+        <label htmlFor="Bill Name">Bill Name</label>
+        <input type="text" name="Bill Name"/>
+        <label htmlFor="Bill Amount">Bill Amount</label>
         <input type="number" name="Bill Amount"/>
         <button type="submit">Submit</button>
       </form>
+      <h2>Bills List</h2>
+      <p>Here are all of your bills</p>
       <ul className="budget-bills-list">
         <li>Internet $65</li>
         <li>Utilities $250</li>
