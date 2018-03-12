87d2a642229f825d3196e22c4d973a3f284255e0
Adrian <adriantoddross@gmail.com>
2018-03-12 15:01:59 -0400

    convert to redux form

--- src/components/bills-checkbox.js
+++ src/components/bills-checkbox.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { reduxForm, Field} from 'redux-form';
+import Input from './input';
+import { required, nonEmpty } from  '../validators'
 
-export default function BillsCheckbox (props) {
+export function BillsCheckbox (props) {
+
   return (
     <div className="budget-bills-checkbox">
-      <h3>What bills have you paid so far?</h3>
-      <select name="Paid Bills" id="">
-      <option value=""></option>
-      <option value="Internet">Internet</option>
-      <option value="Groceries">Groceries</option>
-      <option value="Utilities">Utilities</option>
-      </select>
+      <Field
+        name="paid-bills"
+        label="What bills have you paid so far?"
+        component={Input}
+        element="select"
+        validate={[required, nonEmpty]}
+        >
+        <option value=""></option>
+        <option value="Internet">Internet</option>
+        <option value="Groceries">Groceries</option>
+        <option value="Utilities">Utilities</option>
+      </Field>
     </div>
   );
 }
+
+export default reduxForm({
+  form: 'bills-checkbox'
+})(BillsCheckbox);
