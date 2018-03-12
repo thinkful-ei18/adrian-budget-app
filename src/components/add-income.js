72b7c2112d939a61097cfa30e6273bdf96b7b0ff
Adrian <adriantoddross@gmail.com>
2018-03-12 14:24:38 -0400

    add validator for numbers only fields

--- src/components/add-income.js
+++ src/components/add-income.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import { reduxForm, Field, SubmissionError } from 'redux-form';
+import { reduxForm, Field} from 'redux-form';
 import Input from './input';
-import { required, nonEmpty, maxNumbers } from  '../validators';
+import { required, nonEmpty, numbersOnly } from  '../validators';
 
 
 export function AddIncome (props) {
 return (
- <div className="budget-income">
+  <div className="budget-income">
   <h2>Add your monthly income!</h2>
   <p>Add your expected monthly income.</p>
-    <form onClick={() => console.log('Form clicked!')}>
-      <label htmlFor="Monthly Income">Monthly Income</label>
-      <input type="number" name="Monthly Income"/>
+    <form onClick={(value) => console.log('Monthly income:', value)}>
+      <Field
+        name="monthlyIncome"
+        label="Monthly Income"
+        type="number"
+        component={Input}
+        validate={[required, nonEmpty, numbersOnly]}
+      />
       <button type="submit">Submit</button>
     </form>
- </div>
-);
+  </div>
+  );
 }
 
 
