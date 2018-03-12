80b7466f2b70a79e03bc7456f2297fc62e892a2a
Adrian <adriantoddross@gmail.com>
2018-03-12 11:32:25 -0400

    add static HTML

--- src/components/bills-list.js
+++ src/components/bills-list.js
@@ -2,6 +2,21 @@ import React from 'react';
 
 export default function billsList (props) {
   return (
-
+    <div className="budget-bills">
+      <h2>Add A Bill</h2>
+      <p>Add a name and dollar amount for your monthly bills</p>
+      <form className="add-bill">
+        <label htmlFor="Bill Amont">Bill Amount</label>
+        <input type="number" name="Bill Amount"/>
+        <button type="submit">Submit</button>
+      </form>
+      <ul className="budget-bills-list">
+        <li>Internet $65</li>
+        <li>Utilities $250</li>
+        <li>Groceries $150</li>
+      </ul>
+      <p>Total $465</p>
+      <p>After Bills: $1,535 left</p>
+    </div>
   );
 }
