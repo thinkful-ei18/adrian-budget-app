8ae129d7628c56aa95aac02d463c16d9e1000741
Adrian <adriantoddross@gmail.com>
2018-03-12 10:51:44 -0400

    add className

--- src/components/income.js
+++ src/components/income.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function Income (props) {
 return (
- <div>
+ <div className="budget-income">
   <h2>Add your monthly income!</h2>
   <p>Add your expected monthly income.</p>
     <form onClick={() => console.log('Form clicked!')}>
       <label htmlFor="Monthly Income">Monthly Income</label>
-      <input type="number" name="Monthly Income"  id=""/>
-      <input type="submit" value=""/>
+      <input type="number" name="Monthly Income"/>
+      <button type="submit">Submit</button>
     </form>
  </div>
 );
