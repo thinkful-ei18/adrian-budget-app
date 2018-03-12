2b2870d3498a1ae55708cbedcd3cde55dd84a1d7
Adrian <adriantoddross@gmail.com>
2018-03-12 11:50:49 -0400

    add h3 header

--- src/components/bills-checkbox.js
+++ src/components/bills-checkbox.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function BillsCheckbox (props) {
   return (
     <div className="budget-bills-checkbox">
-      <select name="Bill" id="">What bills have you paid so far?
+      <h3>What bills have you paid so far?</h3>
+      <select name="Paid Bills" id="">
       <option value=""></option>
       <option value="Internet">Internet</option>
       <option value="Groceries">Groceries</option>
