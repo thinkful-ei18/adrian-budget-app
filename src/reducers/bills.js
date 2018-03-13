94dd77c31d8dcf6b37c748dfafc8b2de6200e89c
Adrian <adriantoddross@gmail.com>
2018-03-13 10:35:29 -0400

    export reducer with bills actions

--- src/reducers/bills.js
+++ src/reducers/bills.js
@@ -11,3 +11,31 @@ const initialState = {
   loading: false,
   error: null
 }
+
+export function billsReducer(state=initialState, action) {
+
+  if (action.type === FETCH_BILLS_REQUEST) {
+    return Object.assign({}, state,
+    {
+      loading: true,
+      error: null
+    });
+  }
+  else if (action.type === FETCH_BILLS_SUCCESS) {
+
+    return Object.assign({}, state,
+      {
+      cheeses: action.data,
+      loading: false,
+      error: null
+      });
+  }
+  else if (action.type === FETCH_BILLS_ERROR) {
+    return Object.assign({}, state,
+      {
+        error: action.error,
+        loading: false
+      });
+  }
+  return state;
+}
