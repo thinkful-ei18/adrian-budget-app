6ecbe373bb287ea6051fe961b320b10b6298496d
Adrian <adriantoddross@gmail.com>
2018-03-13 15:28:03 -0400

    figuring out bills total

--- src/components/bills-list.js
+++ src/components/bills-list.js
@@ -8,7 +8,19 @@ export class billsList extends React.Component {
     this.props.dispatch(fetchBills());
     }
 
+    sumOfBills = (obj) => {
+      let sum = 0;
+      for(let value in obj) {
+        if (typeof obj[value] === 'number') {
+          sum+= parseFloat(obj[value]);
+        }
+      }
+      return console.log(sum);
+      // return sum;
+    }
+
   render () {
+
     const allBills = this.props.bills.map((bill, index) =>
   <li key={index} className="budget-list-bill">
     {bill.name}, ${bill.amount}
@@ -19,11 +31,11 @@ export class billsList extends React.Component {
       <div className="budget-list">
         <h2>Bills List</h2>
         <p>Your monthly income is: ${this.props.income}</p>
-        <h3>Here are all of your bills:</h3>
+        <h3>Here are all of your bills ({this.props.bills.length} total):</h3>
         <ul>
           {allBills}
         </ul>
-        <p>Total $465</p>
+        <p>Total ${}</p>
         <p>After Bills: $1,535 left</p>
       </div>
     );
