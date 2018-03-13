882eb2d0387b411636cd0f548340630ce231b1b1
Adrian <adriantoddross@gmail.com>
2018-03-12 21:51:38 -0400

    mentor meeting revisions

--- src/components/bills-list.js
+++ src/components/bills-list.js
@@ -3,18 +3,27 @@ import React from 'react';
 export default class billsList extends React.Component {
 
 
+
   render () {
 
-    const staticBills = ['Internet $65', 'Groceries $250'];
+    // const staticBills = ['Internet $65', 'Groceries $250'];
+    const staticBills = [
+      {name: 'Internet',
+      amount: 65,
+    },
+    {name: 'Groceries',
+    amount: 250,
+    },
+    ];
 
     const bills = staticBills.map((bill, index) =>
-  <li key={index} className="cheese-list-cheese">
-    {bill}
+  <li key={index} className="budget-list-bill">
+    {bill.name}, ${bill.amount}
   </li>
 );
 
     return (
-      <div className="budget-bills-list">
+      <div className="budget-list">
         <h2>Bills List</h2>
         <p>Here are all of your bills</p>
         <ul>
