155f3146c3ce3628f9d5a1b68b9d78e8776b02c5
Adrian <adriantoddross@gmail.com>
2018-03-13 16:49:08 -0400

    convert function into class

--- src/components/add-bill.js
+++ src/components/add-bill.js
@@ -5,32 +5,38 @@ import { required, nonEmpty, numbersOnly } from  '../validators';
 
 
 
-export function addBill (props) {
-  return (
-    <div className="budget-add-bill">
-    <h2>Add A Bill</h2>
-      <p>Add a name and dollar amount for your monthly bills</p>
-      <form className="add-bill">
-      <Field
-        name="name"
-        label="Bill Name"
-        type="text"
-        component={Input}
-        validate={[required, nonEmpty]}
-      />
-      <Field
-        name="amount"
-        label="Bill Amount"
-        type="number"
-        component={Input}
-        validate={[required, nonEmpty, numbersOnly]}
-      />
-      <button type="submit">Submit</button>
+export class addBill extends React.Component {
 
+  onSubmit(values) {
+    // this.props.dispatch(addBill(values));
+  }
 
-      </form>
-    </div>
-  );
+  render () {
+
+    return (
+      <div className="budget-add-bill">
+      <h2>Add A Bill</h2>
+        <p>Add a name and dollar amount for your monthly bills</p>
+        <form className="add-bill">
+        <Field
+          name="name"
+          label="Bill Name"
+          type="text"
+          component={Input}
+          validate={[required, nonEmpty]}
+        />
+        <Field
+          name="amount"
+          label="Bill Amount"
+          type="number"
+          component={Input}
+          validate={[required, nonEmpty, numbersOnly]}
+        />
+        <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default reduxForm({
