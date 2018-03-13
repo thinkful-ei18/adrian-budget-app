0da3639056996a51694ee7600b71104c22833f54
Adrian <adriantoddross@gmail.com>
2018-03-13 12:42:29 -0400

    console.log the income when form is submitted

--- src/components/add-income.js
+++ src/components/add-income.js
@@ -4,25 +4,32 @@ import Input from './input';
 import { required, nonEmpty, numbersOnly } from  '../validators';
 
 
-export function AddIncome (props) {
-return (
-  <div className="budget-income">
-  <h2>Add your monthly income!</h2>
-  <p>Add your expected monthly income.</p>
-    <form onClick={(value) => console.log('Monthly income:', value)}>
-      <Field
-        name="monthlyIncome"
-        label="Monthly Income"
-        type="number"
-        component={Input}
-        validate={[required, nonEmpty, numbersOnly]}
-      />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-  );
+export class AddIncome extends React.Component {
+
+onSubmit(value) {
+  console.log('Monthly income:', value)
 }
 
+render () {
+
+return (
+    <div className="budget-income">
+    <h2>Add your monthly income!</h2>
+    <p>Add your expected monthly income.</p>
+      <form onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
+        <Field
+          name="monthlyIncome"
+          label="Monthly Income"
+          type="number"
+          component={Input}
+          validate={[required, nonEmpty, numbersOnly]}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    );
+  }
+}
 
 export default reduxForm({
   form: 'income'
