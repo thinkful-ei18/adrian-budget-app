b8878b1105b349e50fbf6402280e4e7a05cd0e99
Adrian <adriantoddross@gmail.com>
2018-03-19 16:26:27 -0400

    link to onboarding

--- src/components/signup.js
+++ src/components/signup.js
@@ -30,7 +30,7 @@ export function signUpForm (props) {
         type="text"
         validate={[required, nonEmpty]}
         />
-        <Link to="/bills">
+        <Link to="/onboarding">
           <button type="submit" className="pure-button pure-button-primary">Sign Up</button>
         </Link>
       </form>
