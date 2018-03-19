44dd5b3ff80c51dbe883a2c3970f641aa6fe5d71
Adrian <adriantoddross@gmail.com>
2018-03-19 11:17:19 -0400

    finish static html for log in component

--- src/components/login.js
+++ src/components/login.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import Input from './input';
 import {reduxForm, Field} from 'redux-form';
-import {required, nonEmpty} from 'redux-form';
+import {required, nonEmpty} from '../validators';
 
 export function logInForm (props) {
 return (
   <div>
     <h1>Log In</h1>
     <form className="pure-form pure-form-stacked">
-
+      <Field
+      name="username"
+      label="Username"
+      component={Input}
+      type="text"
+      validate={[required, nonEmpty]}
+      />
+      <Field
+      name="password"
+      label="Password"
+      component={Input}
+      type="text"
+      validate={[required, nonEmpty]}
+      />
+      <button type="submit" className="pure-button pure-button-primary">Sign In</button>
     </form>
   </div>
 );
