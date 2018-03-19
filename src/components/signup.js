fab9e6a63f8bc59f4916df432d2d6f3ce229e88e
Adrian <adriantoddross@gmail.com>
2018-03-19 11:08:08 -0400

    finish static sign-up form

--- src/components/signup.js
+++ src/components/signup.js
@@ -6,13 +6,30 @@ import {required, nonEmpty} from '../validators';
 export function signUpForm (props) {
   return (
     <div>
-      <form className="pure-form pure-form-aligned">
+      <h1>Sign Up</h1>
+      <form className="pure-form pure-form-stacked">
         <Field
         name="fullname"
         label="Full Name"
         component={Input}
         type="text"
+        validate={[required, nonEmpty]}
         />
+        <Field
+        name="username"
+        label="Username"
+        component={Input}
+        type="text"
+        validate={[required, nonEmpty]}
+        />
+        <Field
+        name="password"
+        label="Password"
+        component={Input}
+        type="text"
+        validate={[required, nonEmpty]}
+        />
+        <button type="submit" className="pure-button pure-button-primary">Sign Up</button>
       </form>
     </div>
   );
