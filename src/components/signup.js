ad8e40c1ad0853dd68eafa6c3b57208f83ba544b
Adrian <adriantoddross@gmail.com>
2018-03-19 10:59:57 -0400

    export reduxForm

--- src/components/signup.js
+++ src/components/signup.js
@@ -3,10 +3,21 @@ import Input from './input';
 import {reduxForm, Field} from 'redux-form';
 import {required, nonEmpty} from '../validators';
 
-export default function signUpForm (props) {
+export function signUpForm (props) {
   return (
     <div>
-
+      <form className="pure-form pure-form-aligned">
+        <Field
+        name="fullname"
+        label="Full Name"
+        component={Input}
+        type="text"
+        />
+      </form>
     </div>
   );
 }
+
+export default reduxForm({
+  form: 'signup'
+})(signUpForm);
