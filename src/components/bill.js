552e1ffacc14243ba8f243a894d366648143d80b
Adrian <adriantoddross@gmail.com>
2018-03-19 11:59:19 -0400

    import bill component

--- src/components/bill.js
+++ src/components/bill.js
@@ -4,8 +4,8 @@ import {connect} from 'react-redux';
 export default function Bill (props) {
   return (
     <div className="bill">
-      <h1>{props.bill.name}</h1>
-      <p>{props.bill.amount}</p>
+      <h1>{props.name}</h1>
+      <p>{props.amount}</p>
     </div>,
     <div className="categories">
     <p>Categories will be added soon!</p>
