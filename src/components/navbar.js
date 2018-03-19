10f25fba6307f87c93eb89e5025a4586a21e5c69
Adrian <adriantoddross@gmail.com>
2018-03-19 16:06:36 -0400

    add link to logout

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function navbar (props) {
   return (
     <nav className="pure-menu pure-menu-horizontal">
       <ul className="pure-menu-list">
-        <li className="pure-menu-item"><a href="" class="pure-menu-link">Logout</a></li>
+        <li className="pure-menu-item"><Link to="/">Logout</Link></li>
         <li className="pure-menu-item"><a href="" class="pure-menu-link">Update Your Income</a></li>
         <li className="pure-menu-item"><a href="" class="pure-menu-link">Add A Bill</a></li>
       </ul>
