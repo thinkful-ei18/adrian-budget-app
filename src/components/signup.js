32c7eb856b2a339358533276dcd39ede21d75681
Adrian <adriantoddross@gmail.com>
2018-03-19 10:54:01 -0400

    import all requirements

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Input from './input';
+import {reduxForm, Field} from 'redux-form';
+import {required, nonEmpty} from '../validators';
 
 export default function signUpForm (props) {
   return (
