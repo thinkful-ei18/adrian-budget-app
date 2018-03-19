cc7ca350478a6780a41cc7f7718d81f545a88b55
Adrian <adriantoddross@gmail.com>
2018-03-19 14:53:36 -0400

    test root route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Route, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import AddIncome from './components/add-income';
 import AddBills from './components/add-bill';
@@ -8,23 +8,28 @@ import BillsList from './components/bills-list';
 import BillsCheckbox from './components/bills-checkbox';
 import SignUpForm from './components/signup';
 import LoginForm from './components/login';
+
+
+
 class App extends Component {
   render() {
     return (
-      <div class="pure-g">
-        <div class="pure-u-1-3">
-          <AddIncome />
-          <AddBills />
-        </div>
-        <div class="pure-u-1-3">
-          <BillsList />
-          <BillsCheckbox />
-        </div>
-        <div class="pure-u-1-3">
-          <SignUpForm />
-          <LoginForm />
-        </div>
-      </div>
+      <Switch>
+        <Route exact path='/'>
+          <div className="pure-g">
+            <div className="pure-u-1-3">
+
+            </div>
+            <div className="pure-u-1-3">
+              <LoginForm/>
+            </div>
+            <div className="pure-u-1-3">
+              <SignUpForm/>
+            </div>
+          </div>
+        </Route>
+
+      </Switch>
     );
   }
 }
