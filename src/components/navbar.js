e65ac9b45c8b12c4f99d1663d475fa68d0972847
Adrian <adriantoddross@gmail.com>
2018-03-21 11:30:49 -0400

    move to the right

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,9 +4,9 @@ import './navbar.css';
 
 export default function navbar (props) {
   return (
-    <nav className="pure-menu pure-menu-horizontal">
-      <ul className="pure-menu-list">
-        <li className="pure-menu-item"><Link to="/">Logout</Link></li>
+    <nav className=" navbar pure-menu pure-menu-horizontal">
+      <ul className=" navlist pure-menu-list">
+        <li className="pure-menu-item pure-menu-link"><Link to="/">Logout</Link></li>
         <li className="pure-menu-item"><a href="" className="pure-menu-link">Update Your Income</a></li>
         <li className="pure-menu-item"><a href="" className="pure-menu-link">New Bill</a></li>
       </ul>
