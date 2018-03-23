724af19c66c5465846d0a63b39eb0cbcea75b002
Adrian <adriantoddross@gmail.com>
2018-03-23 01:34:10 -0400

    import and add usersReducer

--- src/store.js
+++ src/store.js
@@ -3,11 +3,13 @@ import {reducer as formReducer} from 'redux-form';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import {billsReducer} from './reducers/bills';
+import {usersReducer} from './reducers/users';
 
 
 
 export default createStore (
   combineReducers({
+    users: usersReducer,
     bills: billsReducer,
     form: formReducer
   }), composeWithDevTools(
