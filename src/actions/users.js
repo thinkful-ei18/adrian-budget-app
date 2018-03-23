188cee067ed9401c178e329c463a01278396430b
Adrian <adriantoddross@gmail.com>
2018-03-23 00:28:54 -0400

    add login actions

--- src/actions/users.js
+++ src/actions/users.js
@@ -17,3 +17,21 @@ export const registerUserError = error => ({
   type: REGISTER_USER_ERROR,
   error
 });
+
+
+export const LOGIN_USER_REQUEST = 'LOGIN_USER_REQUEST';
+export const loginUserRequest = () => ({
+  type: LOGIN_USER_REQUEST
+});
+
+export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
+export const loginUserSuccess = data => ({
+  type: LOGIN_USER_SUCCESS,
+  data
+});
+
+export const LOGIN_USER_ERROR = 'LOGIN_USER_ERROR';
+export const loginUserError = error => ({
+  type: LOGIN_USER_ERROR,
+  error
+});
