81ec8093db16a5beb2c65018a3823a65f8371010
Adrian <adriantoddross@gmail.com>
2018-03-23 16:19:00 -0400

    console log registration values

--- src/components/signup.js
+++ src/components/signup.js
@@ -4,39 +4,45 @@ import {reduxForm, Field} from 'redux-form';
 import {required, nonEmpty} from '../validators';
 import { Link } from 'react-router-dom';
 
-export function signUpForm (props) {
-  return (
-    <div>
-      <h1>Sign Up</h1>
-      <form className="pure-form pure-form-stacked">
-        <Field
-        name="fullname"
-        label="Full Name"
-        component={Input}
-        type="text"
-        validate={[required, nonEmpty]}
-        />
-        <Field
-        name="username"
-        label="Username"
-        component={Input}
-        type="text"
-        validate={[required, nonEmpty]}
-        />
-        <Field
-        name="password"
-        label="Password"
-        component={Input}
-        type="text"
-        validate={[required, nonEmpty]}
-        />
-        <Link to="/onboarding-income">
-          <button type="submit" className="pure-button pure-button-primary">Sign Up</button>
-        </Link>
-      </form>
-    </div>
-  );
+export class signUpForm extends React.Component {
+
+  onSubmit(value) {
+  console.log(value);
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        <form className="pure-form pure-form-stacked" onSubmit={this.props.handleSubmit(value => this.onSubmit(value))}>
+          <Field
+          name="fullname"
+          label="Full Name"
+          component={Input}
+          type="text"
+          validate={[required, nonEmpty]}
+          />
+          <Field
+          name="username"
+          label="Username"
+          component={Input}
+          type="text"
+          validate={[required, nonEmpty]}
+          />
+          <Field
+          name="password"
+          label="Password"
+          component={Input}
+          type="text"
+          validate={[required, nonEmpty]}
+          />
+            <button type="submit" className="pure-button pure-button-primary">Sign Up</button>
+        </form>
+      </div>
+    );
+  }
 }
+
 
 export default reduxForm({
   form: 'signup'
